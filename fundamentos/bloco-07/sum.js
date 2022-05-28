8fa2d23c1269b02e8690fec520377db94fbe2b3a
EdvaldoJose <edvaldojose003@gmail.com>
2022-05-28 19:55:10 -0300

    Atualização de arquivos

---
+++ fundamentos/bloco-07/sum.js
@@ -0,0 +1,12 @@
+
+let salarioBase = 1490.00;
+let gratifSalario = 510.00;
+
+function salario(salarioBase, gratifSalario) {
+  let totalSalario = salarioBase + gratifSalario;
+    for (let total of totalSalario ){
+      totalSalario = total + salario;
+    }
+    // return totalSalario;
+  }
+  console.log(totalSalario());
