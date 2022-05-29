c347afe12a59293bf5c022d672b7b5df601f475f
EdvaldoJose <edvaldojose003@gmail.com>
2022-05-29 14:21:54 -0300

    Atualização

--- fundamentos/bloco-07/sum.js
+++ fundamentos/bloco-07/sum.js
@@ -1,12 +1,58 @@
 
-let salarioBase = 1490.00;
-let gratifSalario = 510.00;
+// let salarioBase = 1490.00;
+// let gratifSalario = 510.00;
 
-function salario(salarioBase, gratifSalario) {
-  let totalSalario = salarioBase + gratifSalario;
-    for (let total of totalSalario ){
-      totalSalario = total + salario;
-    }
-    // return totalSalario;
+// function salario(salarioBase, gratifSalario) {
+//   let totalSalario = salarioBase + gratifSalario;
+//     for (let total of totalSalario ){
+//       totalSalario = total + salario;
+//     }
+//     // return totalSalario;
+//   }
+//   console.log(totalSalario());
+
+let number = 5;
+function fatNamber(fatorial) {
+  for (let index = 0; index < number; index += 1) {
+    fatorial = fatorial * number;
   }
-  console.log(totalSalario());
+    if (number < 0) {
+    console.log("digite apenas números positivos");
+      fotorial = 1;
+    }
+    if (number < number -1) {
+      return fatoral * number;
+    }
+  return fatorial;
+}
+console.log(fatNamber(number));
+
+// soma de todas as idades
+
+let idades = [15, 46, 75, 34, 23];
+let total = 0;
+function idade() {
+  for (let index = 0; index < idades; index += 1) {
+    total = total + idades[index];
+  }
+  console.log(total);
+  return total;
+}
+// idade();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ test_foo.js
@@ -0,0 +1,10 @@
+let foo = 'Olá mundo!';
+foo
+console.log(foo);
+for (let index = 0; index < 5.length; index += 1) {
+for (let index = 0; index < 5; index += 1) { }
+for (let index = 0; index < 5; index += 1) { }
+for (let index = 0; index < 5; index += 1) { }
+for (let index = 0; index < 5; index += 1) {...console.log(index);
+foo
+foo
